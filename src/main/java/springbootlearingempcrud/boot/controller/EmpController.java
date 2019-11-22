98cea5219633a55fb1e4e5fd35f4e33686aaabee
yinqingshan <2262676640@qq.com>
2019-11-22 17:55:08 +0800

    修改日期参数格式转换 V2.0

--- src/main/java/springbootlearingempcrud/boot/controller/EmpController.java
+++ src/main/java/springbootlearingempcrud/boot/controller/EmpController.java
@@ -43,7 +43,6 @@ public class EmpController {
 
     @PostMapping("/emp")
     public String add(Employee employee) {
-        System.out.println(employee);
         employeeDao.save(employee);
         return "redirect:/emps";
     }
